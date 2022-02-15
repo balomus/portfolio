68cfe69843a39264ca91ec66ba23345cf2374784
Stephen Mercier <sjmercier@wcu.edu>
2022-02-15 14:55:07 -0500

    Added Tailwind and some initial styling

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./build/index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'home-image': "url('https://res.cloudinary.com/dbnugenbb/image/upload/v1458915250/apple-desk-laptop-working1280_zesrlm.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
